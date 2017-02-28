73a4d9bca0d638231781dbdfe1bef494683d303a
ShinHo Chul <dsp@ShinHoui-MacBook-Pro.local>
2017-03-01 01:31:17 +0900

    mean 로직 추가 및 data update

--- noti.py
+++ noti.py
@@ -6,7 +6,7 @@ import pandas_datareader.data as web
 from datetime import datetime, timedelta
 import time
 
-stocks = pd.read_csv('stocks_201612.csv', encoding='utf-8')
+stocks = pd.read_csv('kospi_201702.csv')
 codes = stocks.iloc[:,1]
 
 m2 = timedelta( days = 60 )
@@ -14,11 +14,18 @@ end = datetime.now()
 start = end - m2
 
 for code in codes:
-    c = str(code)+'.KS'
-    try:
-        data = web.DataReader( c, 'yahoo', start, end )
-    except:
-        print c, 'is kosdaq'
-    else:
-        print c, 'is success!!'
-    time.sleep(1)
+	c = str(code)
+	c = '0'*(6-len(c))+c+'.KS'
+	try:
+	    data = web.DataReader( c, 'yahoo', start, end )
+	except:
+	    print 'error:', c
+	else:
+	    v = data['Volume']
+	    last = v[-1]
+	    mean = v[:-1].mean()
+	    print c, mean, last
+	    if last > mean*20:
+	    	print 'Found !!!!'
+	
+	time.sleep(1)
