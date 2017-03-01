f1d4b0c3af5d032c7589d989b37d725d54d74517
ShinHo Chul <dsp@ShinHoui-MacBook-Pro.local>
2017-03-01 13:07:52 +0900

    날자비교 및 종가비교 루틴 추가

--- noti.py
+++ noti.py
@@ -5,6 +5,10 @@ import pandas as pd
 import pandas_datareader.data as web
 from datetime import datetime, timedelta
 import time
+import sys
+import traceback
+
+THRESHOLD_RATIO = 10
 
 stocks = pd.read_csv('kospi_201702.csv')
 codes = stocks.iloc[:,1]
@@ -13,19 +17,43 @@ m2 = timedelta( days = 60 )
 end = datetime.now()
 start = end - m2
 
-for code in codes:
+cands = []
+for i, code in enumerate(codes):
 	c = str(code)
 	c = '0'*(6-len(c))+c+'.KS'
 	try:
 	    data = web.DataReader( c, 'yahoo', start, end )
 	except:
-	    print 'error:', c
+	    print 'error in', c
+	    traceback.print_exc()
 	else:
 	    v = data['Volume']
+
+	    lastDay = v.index[-1]
+	    if lastDay.date() != end.date():
+	    	# 마지막날이 오늘이 아니면 오늘장은 쉬는 날임.
+	    	#print "today is off. because last date:", lastDay.date(), 'today:', end.date()
+	    	#sys.exit(0)
+	    	pass
+
 	    last = v[-1]
-	    mean = v[:-1].mean()
-	    print c, mean, last
-	    if last > mean*20:
-	    	print 'Found !!!!'
+	    mean = v[:-1].mean() #워킹데이 기준 59일치 평균
+	    ratio = last / mean
+
+	    # 마지막날 올랐는지 체크
+	    lastCloseDiff = data['Adj Close'].diff()[-1]
+
+	    print i, c, ratio, lastCloseDiff
+	    if ratio > THRESHOLD_RATIO and lastCloseDiff > 0:
+	    	# ratio가 기준을 넘고, 마지막날 주가가 오른 경우
+	    	print 'Found !'
+	    	cands.append({
+	    		'code':c,
+	    		'ratio':ratio,
+	    		'lastCloseDiff':lastCloseDiff
+	    		})
 	
-	time.sleep(1)
+	# 슬립은 필요없어 보임.
+	#time.sleep(1) 
+
+print cands
